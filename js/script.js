f69fac91faff716428e4aa0a6ffd92fb2d1794f1
aCute5 <costantini95dario@gmail.com>
2022-10-27 15:14:38 +0200

    ciclo while aggiunto

---
+++ js/script.js
@@ -0,0 +1,69 @@
+/*
+Consegna:
+Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
+Consigli:
+- Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ciclo while
+- Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all'interno ciclo while
+*/
+
+/*
+const arrGroceryList = [
+	'pomodori',
+	'latte',
+	'patate',
+	'pesce',
+	'cereali',
+	'carne',
+	'dentifricio',
+];
+*/
+
+const arrGroceryList = [];
+
+/*
+let item = prompt('inserisci qualcosa da comprare');
+while (item != '') {
+	arrGroceryList.push(item);
+	item = prompt('inserisci un\'altra cosa da compore'); // variazione della variabile testata
+}
+*/
+let item
+do {
+	item = prompt('inserisci qualcosa da comprare');
+	arrGroceryList.push(item);
+} while (item != ''  && arrGroceryList.length < 5)
+if (!arrGroceryList[arrGroceryList.length - 1]) {
+	arrGroceryList.pop();
+}
+
+const eleGroceryList = document.querySelector('.grocery-list');
+const inputGroceryItem = document.querySelector('#input-grocery');
+const eleGroceryForm = document.querySelector('form');
+
+eleGroceryForm.addEventListener('submit', function(event) {
+	event.preventDefault();
+
+	if (inputGroceryItem.value) {
+		// aggiornare i dati
+		arrGroceryList.push(inputGroceryItem.value);
+
+		// aggiornare l'interfaccia
+		eleGroceryList.innerHTML += `<li>${inputGroceryItem.value}</li>`;
+
+		// svuotare l'input
+		inputGroceryItem.value = '';
+	}
+});
+let i = 0
+while (i < arrGroceryList.length) {
+	eleGroceryList.innerHTML += `<li>${arrGroceryList[i]}</li>`;
+    i++
+}
+
+for (let i = 1; i <= 10; i++) {
+	console.log(i);
+}
+
+console.log('con while')
+
+
